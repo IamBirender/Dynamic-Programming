6166a24a8218548879e0cb5f982623041dbbc0db
IamBirender <birender1332.11@gmail.com>
2020-08-07 00:29:27 +0000

    video 5

---
+++ Knapsack/knapsack01/video_5.py
@@ -0,0 +1,32 @@
+class video_5(object):
+    def __init__(self, weight, capacity, value):
+        self.weight = weight
+        self.capacity = capacity
+        self.value = value
+        self.N = len(weight)
+        self.dp = [[-1 for i in range(capacity+1)] for j in range(self.N+1)]
+    
+    def _get_highest_profit(self):
+        self.__top_down_approach()
+        for l in self.dp:
+            print(l)
+    
+    def __top_down_approach(self):
+        for i in range(self.N+1):
+            for j in range(self.capacity+1):
+                if i == 0:
+                    self.dp[i][j] = 0
+                elif j == 0:
+                    self.dp[i][j] = 0
+                elif self.weight[i-1] < j: 
+                    self.dp[i][j] = max(self.value[i-1] + self.dp[i][j-self.weight[i-1]], self.dp[i][j-1])
+                else:
+                    self.dp[i][j] = self.dp[i][j-1]
+
+weight = [1,3,4,5]
+value = [1,4,5,7]
+capacity = 7
+
+obj = video_5(weight, capacity, value)
+obj._get_highest_profit()
+
