ff13b0759d22d3ce3c868e195abc6d481baff47d
Birender Singh <birender1332.11@gmail.com>
2020-08-15 09:48:57 -0700

    Update longest_common_substring.py

--- Longest-common-subsequence/longest_common_substring.py
+++ Longest-common-subsequence/longest_common_substring.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     def __init__(self, str1, str2):
         self.str1 = str1 
@@ -6,11 +5,13 @@ class Solution(object):
         self.l1 = len(str1)
         self.l2 = len(str2)
         self.memo = [[-1 for i in range(self.l1 + 2)] for j in range(self.l2 + 2)]
+        self.dp = [[-1 for i in range(self.l2+1)] for j in range(self.l1+1)]
     
     def _get_LCS(self):
 #         return self.__recursive_solution(self.l1, self.l2)
-        self.__recursion_with_memo(self.l1, self.l2)
-        for i in self.memo:
+#         self.__recursion_with_memo(self.l1, self.l2)
+        self.__top_down()
+        for i in self.dp:
             print(i)
     
     def __recursive_solution(self, p1, p2):
@@ -34,6 +35,21 @@ class Solution(object):
         else:
             self.memo[p1][p2] = max(self.__recursion_with_memo(p1-1, p2) ,self.__recursion_with_memo(p1, p2-1))
         return self.memo[p1][p2]
+    
+    def __top_down(self):
+        for i in range(self.l1+1):
+            for j in range(self.l2+1):
+                if i == 0 or j == 0:
+                    self.dp[i][j] = 0
+                
+                elif self.str1[i-1] == self.str2[j-1]:
+                    self.dp[i][j] = 1 + self.dp[i-1][j-1]
+                
+                else:
+                    self.dp[i][j] = max(self.dp[i-1][j], self.dp[i][j-1])
+                    
+        return self.dp[-1][-1]
+                    
         
         
 s1 = "abcdgh"
